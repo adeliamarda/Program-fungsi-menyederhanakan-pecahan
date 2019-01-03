70ec148d7bf74facdc634df9ac5f5e8a98ff379d
Adelia Marda Trisno <46318729+adeliamarda@users.noreply.github.com>
2019-01-03 19:54:30 +0700

    Add files via upload

---
+++ Program fungsi menyederhanakan bc menjadi Abc.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void pecahan();
+void main() {
+    pecahan();
+}
+
+void pecahan(){
+    int a,b,l,t;
+    printf("Masukkan pembilang : ");
+    scanf("%d",&a);
+    printf("Masukan penyebut : ");
+    scanf("%d",&b);
+    scanf("%d",&b);
+    t = a/b;
+    l = a%b;
+    if (a%b == 0)
+        printf("Bilangan (%d/%d) \ndi sederhanakan menjadi %d ",a,b,t);
+        else if (a%b != 0)
+            printf("Bilangan (%d/%d) \ndi sederhanakan menjadi (%d %d%d)",a,b,t,l,b);
+
+
+}
